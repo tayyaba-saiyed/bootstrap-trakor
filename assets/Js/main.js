90421edeeb351e0832dd62d4d3c189badc1427bc
tayyaba-saiyed <taytaywondertube@gmail.com>
2024-10-17 12:36:48 +0530

    1st commit

---
+++ assets/Js/main.js
@@ -0,0 +1,31 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    // centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    },
+   
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
